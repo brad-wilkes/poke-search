f2ca6b0c141d8cedb0212d98fb79e9822bef1d03
juppo <aldelta19@gmail.com>
2024-01-14 14:47:32 -0600

    deleting dead code

--- src/stores/atoms.ts
+++ src/stores/atoms.ts
@@ -1,32 +1,6 @@
 import {atom} from 'jotai'
 import {PokemonListArray} from '../types/pokemon'
-// import pokemondata from '../assets/pokemondata.json'
-// import { fetchGistData, gistId } from '../api/endpoints';
 
-
-// export const pokemonAtom:PokemonList = atom([]);
 export const pokemonDataAtom = atom<PokemonListArray>([])
-export const pokemonListAtom = atom({ filter: '' });
+export const pokemonListAtom = atom({ filter: '' } as { filter: string });
 export const textAtom= atom('hello')
-export const uppercaseAtom = atom(
-    (get) => get(textAtom).toUpperCase()
-)
-
-// export async function fetchDataAndPopulateAtom() {
-//     try {
-//       const gistData = await fetchGistData(gistId);
-//       const parsedData = JSON.parse(gistData);
-
-//       if (Array.isArray(parsedData)) {
-//         // If the parsed data is an array, iterate through it and update the atom
-//         const setPokemonList = useSetAtom(pokemonListAtom);
-//         setPokemonList(() => parsedData);
-//       } else {
-//         console.error('Parsed data is not an array of Pokemon.');
-//       }
-
-//     } catch (error) {
-//       console.error('Error:', error);
-//       throw error;
-//     }
-//   }
