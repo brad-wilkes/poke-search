a62552a937cd8fc0c5c23155f60e4d257d476446
rbwilkes@yahoo.com <rbwilkes@yahoo.com>
2024-09-29 13:56:12 -0500

    remove language bs from typing

--- src/stores/atoms.ts
+++ src/stores/atoms.ts
@@ -3,3 +3,12 @@ import {PokemonList} from '../types/pokemon'
 
 export const pokemonDataAtom = atom<PokemonList>([])
 export const pokemonFilterAtom = atom<string>('');
+export const filteredPokemonDataAtom = atom<PokemonList>((get) => {
+    const filter = get(pokemonFilterAtom).trim().toLowerCase();
+    const data = get(pokemonDataAtom);
+    return filter === ''
+      ? data
+      : data.filter((pokemon) =>
+          pokemon.name.toLowerCase().includes(filter)
+        );
+  });
