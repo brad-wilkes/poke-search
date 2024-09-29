e941bbe92b5863817cd949ebd0c4e6f97473a25e
rbwilkes@yahoo.com <rbwilkes@yahoo.com>
2024-09-29 12:46:06 -0500

    update list render component

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,27 +1,38 @@
+import { useEffect } from 'react';
 import { useAtom } from 'jotai';
-import pokemondata from '../assets/pokemondata.json'
 import { pokemonDataAtom, pokemonFilterAtom } from '../stores/atoms';
+import { fetchPokemonList } from '../api/endpoints';
 
 export function PokemonList() {
+  const [pokemonData, setPokemonData] = useAtom(pokemonDataAtom);
+  const [pokemonFilter] = useAtom(pokemonFilterAtom);
 
-    const [pokemonData] = useAtom(pokemonDataAtom);
-    const [pokemonFilter] = useAtom(pokemonFilterAtom);
+  useEffect(() => {
+    const fetchAndSetPokemonList = async () => {
+      try {
+        const data = await fetchPokemonList();
+        setPokemonData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    const filteredPokemon = pokemonFilter.filter.trim() === ''
+    fetchAndSetPokemonList();
+  }, [setPokemonData]);
+
+  const filteredPokemon = pokemonFilter.trim() === ''
     ? pokemonData
-    : pokemondata.filter((pokemon) => {
-        return pokemon.name.english.toLowerCase().includes(pokemonFilter.filter.toLowerCase());
-    });
+    : pokemonData.filter((pokemon) => {
+        return pokemon.name.english?.toLowerCase().includes(pokemonFilter.toLowerCase());
+      });
 
   return (
-
     <div>
       {filteredPokemon.map((pokemon, index) => (
         <div key={index}>
-            <hr />
-          {/* <p>Id: {pokemon.id}   </p> */}
-          <p>Name: {pokemon.name.english}   </p>
-          <p>Type: {Array.isArray(pokemon.type) ? pokemon.type.join(', ') : pokemon.type}    </p>
+          <hr />
+          <p>Name: {pokemon.name.english}</p>
+          <p>Type: {Array.isArray(pokemon.type) ? pokemon.type.join(', ') : pokemon.type}</p>
         </div>
       ))}
       <hr />
