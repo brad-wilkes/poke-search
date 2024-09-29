4a4535c4477121f391924ca56244d50083136a59
rbwilkes@yahoo.com <rbwilkes@yahoo.com>
2024-09-29 11:32:42 -0500

    final changes before api refactor

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,14 +1,18 @@
-/**    CORS ISSUE
-The gist server is not allow requests from localhost:5173... so this is a deadend for now
+import { Pokemon } from '../types/pokemon';
 
-export const gistId = '23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823';
 
-export async function fetchGistData(gistId: any) {
-    const response = await fetch(`https://api.githubusercontent.com/jherr/${gistId}/pokemon.json`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch Gist data');
-    }
-    const gistData = await response.json();
-    return gistData.files['data.json'].content;
+export const fetchPokemonList = async () => {
+  const response = await fetch('/pokesearch/list/');
+  if (!response.ok) {
+      throw new Error('Failed to fetch Pokémon list');
   }
-*/
+  return response.json();
+};
+
+export const searchPokemon = async (query: string) => {
+  const response = await fetch(`/pokesearch/search/?q=${query}`);
+  if (!response.ok) {
+      throw new Error('Failed to search Pokémon');
+  }
+  return response.json();
+};
